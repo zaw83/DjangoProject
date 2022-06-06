836bde57975993c8a674e5e1b851bfb08f1131e6
zaw83 <67044969+zaw83@users.noreply.github.com>
2022-06-06 08:17:05 +0630

    First Commit

---
+++ projects/WZ_APP/WZ_APP/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for WZ_APP project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WZ_APP.settings')
+
+application = get_wsgi_application()
